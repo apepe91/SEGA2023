2916d9c1f9f7844a486637fbb3110018c0612079
GianSteve <gianmarcomelito@gmail.com>
2023-06-14 14:22:35 +0200

    pygpc implemented successfully

--- sa/pygpc_sa_KW.py
+++ sa/pygpc_sa_KW.py
@@ -2,138 +2,142 @@
 Algorithm: Static_IO
 ==============================
 """
+
+
 # Windows users have to encapsulate the code into a main function to avoid multiprocessing errors.
-# def main():
-import pygpc
-import numpy as np
-import matplotlib
+def main():
+    import pygpc
+    import numpy as np
+    import matplotlib
 
-# matplotlib.use("Qt5Agg")
+    # matplotlib.use("Qt5Agg")
 
-from collections import OrderedDict
+    from collections import OrderedDict
 
-fn_results = "tmp/static_IO"  # filename of output
-save_session_format = (
-    ".pkl"  # file format of saved gpc session ".hdf5" (slow) or ".pkl" (fast)
-)
-np.random.seed(1)
+    fn_results = "tmp/static_IO"  # filename of output
+    save_session_format = (
+        ".pkl"  # file format of saved gpc session ".hdf5" (slow) or ".pkl" (fast)
+    )
+    np.random.seed(1)
 
-# %%
-# Setup input and output data
-# ----------------------------------------------------------------------------------------------------------------
+    # %%
+    # Setup input and output data
+    # ----------------------------------------------------------------------------------------------------------------
 
-# We artificially generate some coordinates for the input data the user has to provide where the model was sampled
-# LOAD YOUR SAMPLING POINTS IN THE NORMAL MODEL SPACE HERE (NO NORMALIZATION)
-n_grid = 150
-x1 = np.random.rand(n_grid) * 0.8 + 1.2
-x2 = 1.25
-x3 = np.random.rand(n_grid) * 0.6
+    # We artificially generate some coordinates for the input data the user has to provide where the model was sampled
+    # LOAD YOUR SAMPLING POINTS IN THE NORMAL MODEL SPACE HERE (NO NORMALIZATION)
+    n_grid = 150
+    x1 = np.random.rand(n_grid) * 0.8 + 1.2
+    x2 = 1.25
+    x3 = np.random.rand(n_grid) * 0.6
 
-# define the properties of the random variables
-# DEFINE YOUR 4 RANDOM VARIABLES HERE
-parameters = OrderedDict()
-parameters["x1"] = pygpc.Beta(
-    pdf_shape=[1, 1], pdf_limits=[1.2, 2]
-)  # uniform distribution
-parameters["x3"] = pygpc.Norm(pdf_shape=[1, 0.1])  # normal distribution
+    # define the properties of the random variables
+    # DEFINE YOUR 4 RANDOM VARIABLES HERE
+    parameters = OrderedDict()
+    parameters["x1"] = pygpc.Beta(
+        pdf_shape=[1, 1], pdf_limits=[1.2, 2]
+    )  # uniform distribution
+    parameters["x3"] = pygpc.Norm(pdf_shape=[1, 0.1])  # normal distribution
 
-# generate a grid object from the input data
-# my pygpc.RandomGrid does not accept coords, but options={"n_grid": n_grid, "seed": 1}
-grid = pygpc.RandomGrid(parameters_random=parameters, coords=np.vstack((x1, x3)).T)
-print(grid)
-# get output data (here: Peaks function)
-# LOAD YOUR RESULTS HERE shape: [150 x 2]
-results = (
-    3.0 * (1 - x1) ** 2.0 * np.exp(-(x1**2) - (x3 + 1) ** 2)
-    - 10.0 * (x1 / 5.0 - x1**3 - x3**5) * np.exp(-(x1**2) - x3**2)
-    - 1.0 / 3 * np.exp(-((x1 + 1) ** 2) - x3**2)
-) + x2
-# results = np.hstack((results[:, np.newaxis], 2*results[:, np.newaxis]))
-results = results[:, np.newaxis]
+    # generate a grid object from the input data
+    # my pygpc.RandomGrid does not accept coords, but options={"n_grid": n_grid, "seed": 1}
+    grid = pygpc.RandomGrid(parameters_random=parameters, coords=np.vstack((x1, x3)).T)
+    print(grid)
+    # get output data (here: Peaks function)
+    # LOAD YOUR RESULTS HERE shape: [150 x 2]
+    results = (
+        3.0 * (1 - x1) ** 2.0 * np.exp(-(x1**2) - (x3 + 1) ** 2)
+        - 10.0 * (x1 / 5.0 - x1**3 - x3**5) * np.exp(-(x1**2) - x3**2)
+        - 1.0 / 3 * np.exp(-((x1 + 1) ** 2) - x3**2)
+    ) + x2
+    # results = np.hstack((results[:, np.newaxis], 2*results[:, np.newaxis]))
+    results = results[:, np.newaxis]
 
-# %%
-# Setting up the algorithm
-# ------------------------
+    # %%
+    # Setting up the algorithm
+    # ------------------------
 
-# gPC options
-options = dict()
-options["method"] = "reg"
-options["solver"] = "LarsLasso"
-options["settings"] = None
-options["order"] = [9, 9]
-options["order_max"] = 9
-options["interaction_order"] = 2  # 2 or 3 are sufficient
-options["error_type"] = "loocv"
-options["n_samples_validation"] = None
-options["fn_results"] = fn_results
-options["save_session_format"] = save_session_format
-options["backend"] = "omp"
-options["verbose"] = True
+    # gPC options
+    options = dict()
+    options["method"] = "reg"
+    options["solver"] = "LarsLasso"
+    options["settings"] = None
+    options["order"] = [9, 9]
+    options["order_max"] = 9
+    options["interaction_order"] = 2  # 2 or 3 are sufficient
+    options["error_type"] = "loocv"
+    options["n_samples_validation"] = None
+    options["fn_results"] = fn_results
+    options["save_session_format"] = save_session_format
+    options["backend"] = "omp"
+    options["verbose"] = True
 
-# estimate the number of gPC coefficients
-n_coeffs = pygpc.get_num_coeffs_sparse(
-    order_dim_max=options["order"],
-    order_glob_max=options["order_max"],
-    order_inter_max=options["interaction_order"],
-    dim=len(parameters),
-    order_inter_current=None,
-    order_glob_max_norm=1.0,
-)
+    # estimate the number of gPC coefficients
+    n_coeffs = pygpc.get_num_coeffs_sparse(
+        order_dim_max=options["order"],
+        order_glob_max=options["order_max"],
+        order_inter_max=options["interaction_order"],
+        dim=len(parameters),
+        order_inter_current=None,
+        order_glob_max_norm=1.0,
+    )
 
-# n_coeffs = pygpc.get_num_coeffs_sparse(order_dim_max=[6,10,15,19],
-#                                        order_glob_max=5,
-#                                        order_inter_max=2,
-#                                        dim=4,
-#                                        order_inter_current=None,
-#                                        order_glob_max_norm=1.0)
+    # n_coeffs = pygpc.get_num_coeffs_sparse(order_dim_max=[6,10,15,19],
+    #                                        order_glob_max=5,
+    #                                        order_inter_max=2,
+    #                                        dim=4,
+    #                                        order_inter_current=None,
+    #                                        order_glob_max_norm=1.0)
 
-# define algorithm
-algorithm = pygpc.Static_IO(
-    parameters=parameters, options=options, grid=grid, results=results
-)
+    # define algorithm
+    algorithm = pygpc.Static_IO(
+        parameters=parameters, options=options, grid=grid, results=results
+    )
 
-# %%
-# Running the gpc
-# ---------------
+    # %%
+    # Running the gpc
+    # ---------------
 
-# initialize gPC Session
-session = pygpc.Session(algorithm=algorithm)
+    # initialize gPC Session
+    session = pygpc.Session(algorithm=algorithm)
 
-# run gPC algorithm
-session, coeffs, results = session.run()
+    # run gPC algorithm
+    session, coeffs, results = session.run()
 
-# %%
-# Postprocessing
-# --------------
+    # %%
+    # Postprocessing
+    # --------------
 
-# read session
-# session = pygpc.read_session(fname=session.fn_session, folder=session.fn_session_folder)
+    # read session
+    # session = pygpc.read_session(fname=session.fn_session, folder=session.fn_session_folder)
 
-# Post-process gPC
-pygpc.get_sensitivities_hdf5(
-    fn_gpc=options["fn_results"],
-    output_idx=None,
-    calc_sobol=True,
-    calc_global_sens=True,
-    calc_pdf=True,
-    algorithm="standard",
-)
+    # Post-process gPC
+    pygpc.get_sensitivities_hdf5(
+        fn_gpc=options["fn_results"],
+        output_idx=None,
+        calc_sobol=True,
+        calc_global_sens=True,
+        calc_pdf=True,
+        algorithm="standard",
+    )
 
-# plot gPC approximation and IO data
-pygpc.plot_gpc(
-    session=session,
-    coeffs=coeffs,
-    random_vars=["x1", "x3"],
-    output_idx=0,
-    n_grid=[100, 100],
-    coords=grid.coords,
-    results=results,
-    fn_out=None,
-)
+    # plot gPC approximation and IO data
+    pygpc.plot_gpc(
+        session=session,
+        coeffs=coeffs,
+        random_vars=["x1", "x3"],
+        output_idx=0,
+        n_grid=[100, 100],
+        coords=grid.coords,
+        results=results,
+        fn_out=None,
+    )
 
-sobol, gsens = pygpc.get_sens_summary(
-    fn_results, parameters, "/home/kporzig/Desktop/sens_test.txt"
-)
+    sobol, gsens = pygpc.get_sens_summary(
+        fn_results, parameters, "/home/kporzig/Desktop/sens_test.txt"
+    )
+
+
 # pygpc.plot_sens_summary(sobol, gsens, qois=np.arange(2), multiple_qoi=True, results=results)
 
 # pygpc.plot_sens_summary(sobol, gsens)
@@ -141,5 +145,5 @@ sobol, gsens = pygpc.get_sens_summary(
 # You need to insert an if __name__ == '__main__': guard in the main module to avoid
 # creating subprocesses recursively.
 #
-# if __name__ == '__main__':
-#     main()
+if __name__ == "__main__":
+    main()
--- sa/sa_pygpc.py
+++ sa/sa_pygpc.py
@@ -1,73 +1,103 @@
-import math
-import pygpc
-import pandas as pd
-import numpy as np
-import matplotlib
-from collections import OrderedDict
+def main():
+    import pygpc
+    import pandas as pd
+    import numpy as np
+    from collections import OrderedDict
 
-save_session_format = (
-    ".pkl"  # file format of saved gpc session ".hdf5" (slow) or ".pkl" (fast)
-)
+    fn_results = "tmp/static_IO_gm"  # filename of output
+    save_session_format = (
+        ".pkl"  # file format of saved gpc session ".hdf5" (slow) or ".pkl" (fast)
+    )
 
-# load samples
-samplesXLSX = pd.read_excel("sa/01_in_ishigami1000.xlsx")
-samples = samplesXLSX.loc[:, ["x1", "x2", "x3"]].to_numpy()
-modelEval = samplesXLSX.loc[:, "out1"].to_numpy()
+    # load samples
+    samplesXLSX = pd.read_excel("sa/01_in_ishigami1000.xlsx")
+    samples = samplesXLSX.loc[:, ["x1", "x2", "x3"]].to_numpy()
 
-Ns = len(samples)
-M_dim = len(samples.T)
-# print(samples.T[0])
+    Ns = len(samples)
+    M_dim = len(samples.T)
+    # print(samples.T[0])
 
-# define input pdfs
-parameters = OrderedDict()
-parameters["x1"] = pygpc.Beta(
-    pdf_shape=[1, 1], pdf_limits=[min(samples.T[0]), max(samples.T[0])]
-)  # uniform distribution
-parameters["x2"] = pygpc.Beta(
-    pdf_shape=[1, 1], pdf_limits=[min(samples.T[1]), max(samples.T[1])]
-)  # uniform distribution
-parameters["x3"] = pygpc.Beta(
-    pdf_shape=[1, 1], pdf_limits=[min(samples.T[2]), max(samples.T[2])]
-)  # uniform distribution
+    # define input pdfs
+    parameters = OrderedDict()
+    parameters["x1"] = pygpc.Beta(
+        pdf_shape=[1, 1], pdf_limits=[min(samples.T[0]), max(samples.T[0])]
+    )  # uniform distribution
+    parameters["x2"] = pygpc.Beta(
+        pdf_shape=[1, 1], pdf_limits=[min(samples.T[1]), max(samples.T[1])]
+    )  # uniform distribution
+    parameters["x3"] = pygpc.Beta(
+        pdf_shape=[1, 1], pdf_limits=[min(samples.T[2]), max(samples.T[2])]
+    )  # uniform distribution
 
-# CREATE A GRID USING YOUR SAMPLING POINTS
-grid = pygpc.RandomGrid(parameters_random=parameters, options={"n_grid": Ns, "seed": 1})
+    # CREATE A GRID USING YOUR SAMPLING POINTS
+    grid = pygpc.RandomGrid(
+        parameters_random=parameters,
+        coords=np.vstack((samples.T[0], samples.T[1], samples.T[2])).T,
+    )
+    # grid = pygpc.RandomGrid(
+    #     parameters_random=parameters, options={"n_grid": Ns, "seed": 1}
+    # )
+
+    modelEval = samplesXLSX.loc[:, "out1"].to_numpy()
+    results = modelEval[:, np.newaxis]
+
+    # gPC options
+    options = dict()
+    options["method"] = "reg"
+    options["solver"] = "LarsLasso"
+    options["settings"] = None
+    options["order"] = [10, 10, 10]
+    options["order_max"] = 10
+    options["interaction_order"] = M_dim
+    options["error_type"] = "loocv"
+    options["n_samples_validation"] = None
+    options["fn_results"] = fn_results
+    options["save_session_format"] = save_session_format
+    options["backend"] = "omp"
+    options["verbose"] = True
+
+    # estimate the number of gPC coefficients
+    n_coeffs = pygpc.get_num_coeffs_sparse(
+        order_dim_max=options["order"],
+        order_glob_max=options["order_max"],
+        order_inter_max=options["interaction_order"],
+        dim=len(parameters),
+        order_inter_current=None,
+        order_glob_max_norm=1.0,
+    )
+    print("n_coeffs:", n_coeffs)  # same as UQtab
+
+    # define algorithm
+    algorithm = pygpc.Static_IO(
+        parameters=parameters, options=options, grid=grid, results=results
+    )
+
+    # initialize gPC Session
+    session = pygpc.Session(algorithm=algorithm)
+
+    # run gPC algorithm
+    session, coeffs, results = session.run()
+
+    pygpc.get_sensitivities_hdf5(
+        fn_gpc=options["fn_results"],
+        output_idx=None,
+        calc_sobol=True,
+        calc_global_sens=True,
+        calc_pdf=True,
+        algorithm="standard",
+    )
+
+    pygpc.plot_gpc(
+        session=session,
+        coeffs=coeffs,
+        random_vars=["x1", "x2", "x3"],
+        output_idx=0,
+        n_grid=[Ns, Ns],
+        coords=grid.coords,
+        results=results,
+        fn_out=None,
+    )
 
 
-# gPC options
-options = dict()
-options["method"] = "reg"
-options["solver"] = "LarsLasso"
-options["settings"] = None
-options["order"] = [10, 10, 10]
-options["order_max"] = 10
-options["interaction_order"] = 3
-options["error_type"] = "loocv"
-options["n_samples_validation"] = None
-options["fn_results"] = modelEval
-options["save_session_format"] = save_session_format
-options["backend"] = "omp"
-options["verbose"] = True
-
-# estimate the number of gPC coefficients
-n_coeffs = pygpc.get_num_coeffs_sparse(
-    order_dim_max=options["order"],
-    order_glob_max=options["order_max"],
-    order_inter_max=options["interaction_order"],
-    dim=len(parameters),
-    order_inter_current=None,
-    order_glob_max_norm=1.0,
-)
-print(n_coeffs)  # same as UQtab
-
-# define algorithm
-algorithm = pygpc.Static_IO(
-    parameters=parameters, options=options, grid=grid, results=modelEval
-)
-
-# initialize gPC Session
-session = pygpc.Session(algorithm=algorithm)
-
-# run gPC algorithm
-session, coeffs, results = session.run()
-print(session)
+if __name__ == "__main__":
+    main()
