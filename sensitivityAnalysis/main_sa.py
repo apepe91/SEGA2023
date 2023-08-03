838e0d4b9b674a9f0c946f585a2f8f6ee3debd99
GianSteve <gianmarcomelito@gmail.com>
2023-08-03 15:35:53 +0200

    clean

--- sensitivityAnalysis/main_sa.py
+++ sensitivityAnalysis/main_sa.py
@@ -36,7 +36,6 @@ class SEGAsensitivity(object):
 
 
 class SEGAcriteria(object):
-
     def computeP1(sensitivityIndices, inputDim):
         p1Sum = 0
         for index in sensitivityIndices.sensAnalysis[0].indices[0].firstSobol:
@@ -94,38 +93,38 @@ class SEGAcriteria(object):
         )
 
 
-p3_phase2 = pd.read_excel("test_fun/pj_phase2_SEGA23.xlsx")
-p3values_test = np.transpose(
-    p3_phase2.loc[:, ["tpvagenas", "iwm", "tpvagenas_inv", "iwm_inv"]].to_numpy())
-p3_tpvagenas = SEGAcriteria.computePJ(p3values_test[0], p3values_test[2])
-p3_iwm = SEGAcriteria.computePJ(p3values_test[1], p3values_test[3])
-print("pJ_tpvagens:", p3_tpvagenas)
-print("pJ_iwm:", p3_iwm)
-print("========================")
+# p3_phase2 = pd.read_excel("test_fun/pj_phase2_SEGA23.xlsx")
+# p3values_test = np.transpose(
+#     p3_phase2.loc[:, ["tpvagenas", "iwm", "tpvagenas_inv", "iwm_inv"]].to_numpy())
+# p3_tpvagenas = SEGAcriteria.computePJ(p3values_test[0], p3values_test[2])
+# p3_iwm = SEGAcriteria.computePJ(p3values_test[1], p3values_test[3])
+# print("pJ_tpvagens:", p3_tpvagenas)
+# print("pJ_iwm:", p3_iwm)
+# print("========================")
 
-p3_phase2 = pd.read_excel("test_fun/p3_phase2_SEGA23.xlsx")
-p3values_test = np.transpose(
-    p3_phase2.loc[:, ["tpvagenas", "iwm", "sunshine", "test"]].to_numpy())
-p3_tpvagenas = SEGAcriteria.computeP3(p3values_test[0])
-p3_iwm = SEGAcriteria.computeP3(p3values_test[1])
-p3_sunshine = SEGAcriteria.computeP3(p3values_test[2])
-p3_test = SEGAcriteria.computeP3(p3values_test[3])
-print("p3_hd_tpvagens:", p3_tpvagenas)
-print("p3_hd_iwm:", p3_iwm)
-print("p3_hd_sunshine:", p3_sunshine)
-print("p3_hd_test:", p3_test)
-print("========================")
+# p3_phase2 = pd.read_excel("test_fun/p3_phase2_SEGA23.xlsx")
+# p3values_test = np.transpose(
+#     p3_phase2.loc[:, ["tpvagenas", "iwm", "sunshine", "test"]].to_numpy())
+# p3_tpvagenas = SEGAcriteria.computeP3(p3values_test[0])
+# p3_iwm = SEGAcriteria.computeP3(p3values_test[1])
+# p3_sunshine = SEGAcriteria.computeP3(p3values_test[2])
+# p3_test = SEGAcriteria.computeP3(p3values_test[3])
+# print("p3_hd_tpvagens:", p3_tpvagenas)
+# print("p3_hd_iwm:", p3_iwm)
+# print("p3_hd_sunshine:", p3_sunshine)
+# print("p3_hd_test:", p3_test)
+# print("========================")
 
-p4_phase2 = pd.read_excel("test_fun/p4_phase2_SEGA23.xlsx")
-p4values_test = np.transpose(
-    p4_phase2.loc[:, ["tpvagenas", "iwm", "sunshine", "test"]].to_numpy())
-p4_tpvagenas = SEGAcriteria.computeP4(p4values_test[0])
-p4_iwm = SEGAcriteria.computeP4(p4values_test[1])
-p4_sunshine = SEGAcriteria.computeP4(p4values_test[2])
-p4_test = SEGAcriteria.computeP4(p3values_test[3])
-print("p4_dice_tpvagens:", p4_tpvagenas)
-print("p4_dice_iwm:", p4_iwm)
-print("p4_dice_sunshine:", p4_sunshine)
-print("p4_dice_test:", p4_test)
+# p4_phase2 = pd.read_excel("test_fun/p4_phase2_SEGA23.xlsx")
+# p4values_test = np.transpose(
+#     p4_phase2.loc[:, ["tpvagenas", "iwm", "sunshine", "test"]].to_numpy())
+# p4_tpvagenas = SEGAcriteria.computeP4(p4values_test[0])
+# p4_iwm = SEGAcriteria.computeP4(p4values_test[1])
+# p4_sunshine = SEGAcriteria.computeP4(p4values_test[2])
+# p4_test = SEGAcriteria.computeP4(p3values_test[3])
+# print("p4_dice_tpvagens:", p4_tpvagenas)
+# print("p4_dice_iwm:", p4_iwm)
+# print("p4_dice_sunshine:", p4_sunshine)
+# print("p4_dice_test:", p4_test)
 
 sensitivity = SEGAsensitivity()
